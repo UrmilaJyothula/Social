d3f1557e33ee548d687f03cdab5efc91298945c8
UrmilaJyothula <35899608+UrmilaJyothula@users.noreply.github.com>
2018-11-26 18:26:54 +0100

    Create loadfollow.php

---
+++ loadfollow.php
@@ -0,0 +1,18 @@
+<html>
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8">	
+		<link rel="stylesheet" type="text/css" href="css/loadimages.css">
+	</head>
+	
+	<body>		
+			<div class="outertube">
+				<center>
+					<?php
+						if(isset($_POST["name"])){
+							echo "<img    border-radius: 5px; cursor: pointer; transition: 0.3s; margin-top: 15px; src=\"images/follow.png\" height=\"456\" width=\"360\">";
+						}
+					?>
+				</center>
+			</div>
+	</body>
+</html>
